76682230a9272588b67a1e8f060fb6fec3950503
Nishimura Ken <daiconman22@gmail.com>
2015-04-07 13:24:55 +0900

    initial commit

---
+++ ObjcUML/origin.js
@@ -0,0 +1,8 @@
+   var dependencies = {
+    links:
+      [
+            { "source" : "AppDelegate", "dest" : "SystemMonitor" },
+            { "source" : "main", "dest" : "AppDelegate" },
+         ]
+    }
+  ;
